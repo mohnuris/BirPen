cb0df9e3a528ad34918e3aa22f9c72a431f14ddd
nuris.ft.unuja@gmail.com <nuris.ft.unuja@gmail.com>
2020-03-17 06:21:42 +0700

    modal

--- application/views/V_Dashboard.php
+++ application/views/V_Dashboard.php
@@ -9,21 +9,212 @@
 
                                                   </div>
                                         </div>
-                                        <!-- <canvas id="coin_sales2" height="40"></canvas> -->
+                                        <!-- Modal -->
+                                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                  <div class="modal-dialog" role="document">
+                                                            <div class="modal-content">
+                                                                      <div class="modal-header">
+                                                                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                                                <form>
+                                                                                          <div class="form-group">
+                                                                                                    <label for="exampleFormControlInput1">Email address</label>
+                                                                                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                                                                                          </div>
+                                                                                          <div class="form-group">
+                                                                                                    <label for="exampleFormControlSelect1">Example select</label>
+                                                                                                    <select class="form-control" id="exampleFormControlSelect1">
+                                                                                                              <option>1</option>
+                                                                                                              <option>2</option>
+                                                                                                              <option>3</option>
+                                                                                                              <option>4</option>
+                                                                                                              <option>5</option>
+                                                                                                    </select>
+                                                                                          </div>
+                                                                                          <div class="form-group">
+                                                                                                    <label for="exampleFormControlSelect2">Example multiple select</label>
+                                                                                                    <select multiple class="form-control" id="exampleFormControlSelect2">
+                                                                                                              <option>1</option>
+                                                                                                              <option>2</option>
+                                                                                                              <option>3</option>
+                                                                                                              <option>4</option>
+                                                                                                              <option>5</option>
+                                                                                                    </select>
+                                                                                          </div>
+                                                                                          <div class="form-group">
+                                                                                                    <label for="exampleFormControlTextarea1">Example textarea</label>
+                                                                                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                                                                          </div>
+                                                                                </form>
+
+                                                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                                          <span aria-hidden="true">&times;</span>
+                                                                                </button>
+                                                                      </div>
+                                                                      <div class="modal-body">
+                                                                                ...
+                                                                      </div>
+                                                                      <div class="modal-footer">
+                                                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                                                <button type="button" class="btn btn-primary">Save changes</button>
+                                                                      </div>
+                                                            </div>
+                                                  </div>
+                                        </div>
+                                        <!-- Button trigger modal -->
+                                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                                  Launch demo modal
+                                        </button>
+                                        <!-- Modal -->
+                                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                  <div class="modal-dialog" role="document">
+                                                            <div class="modal-content">
+                                                                      <div class="modal-header">
+                                                                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                                          <span aria-hidden="true">&times;</span>
+                                                                                </button>
+                                                                      </div>
+                                                                      <div class="modal-body">
+                                                                                ...
+                                                                      </div>
+                                                                      <div class="modal-footer">
+                                                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                                                <button type="button" class="btn btn-primary">Save changes</button>
+                                                                      </div>
+                                                            </div>
+                                                  </div>
+                                        </div>
+                                        <!-- Button trigger modal -->
+                                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                                  Launch demo modal
+                                        </button>
+                                        <!-- Modal -->
+                                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                  <div class="modal-dialog" role="document">
+                                                            <div class="modal-content">
+                                                                      <div class="modal-header">
+                                                                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                                          <span aria-hidden="true">&times;</span>
+                                                                                </button>
+                                                                      </div>
+                                                                      <div class="modal-body">
+                                                                                ...
+                                                                      </div>
+                                                                      <div class="modal-footer">
+                                                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                                                <button type="button" class="btn btn-primary">Save changes</button>
+                                                                      </div>
+                                                            </div>
+                                                  </div>
+                                        </div>
                               </div>
 
 
                               <div class="col-xl-3 col-md-6 mb-4">
-                                        <!-- <div class="card border-left-primary shadow h-4"> -->
-                                        <!-- <div class="card-body bg-primary"> -->
-
-                                        <!-- </div> -->
-                                        <!-- </div> -->
                               </div>
                               <br>
                               <h1>Selamat datang di website</h1>
                     </div>
           </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 </div>
 
