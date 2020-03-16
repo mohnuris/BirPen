2a55c78500fb36e758112174015713c0baa2cee9
nuris.ft.unuja@gmail.com <nuris.ft.unuja@gmail.com>
2020-03-17 03:25:05 +0700

    menambah button

--- application/views/V_Dashboard.php
+++ application/views/V_Dashboard.php
@@ -3,12 +3,10 @@
                     <div class="row mt-5">
                               <div class="single-report mb-xs-30">
                                         <div class="s-report-inner pr--20 pt--30 mb-3">
-                                                  <div class="icon"><i class="fa fa-user-plus"></i></div>
+
                                                   <div class="s-report-title d-flex justify-content-between">
-                                                            <!-- <a class="header-title mb-0" href="<?= base_url('admin/users') ?>">USERS</a> --
-                                                  </div>
-                                                  <div class="d-flex justify-content-between pb-2">
-                                                            <!-- <h2><?= $us ?></h2> -->
+                                                            <button type="button" class="btn btn-primary fa fa-user-plus" hr> Pengguna</button>
+
                                                   </div>
                                         </div>
                                         <!-- <canvas id="coin_sales2" height="40"></canvas> -->
@@ -16,13 +14,14 @@
 
 
                               <div class="col-xl-3 col-md-6 mb-4">
-                                        <div class="card border-left-primary shadow h-4">
-                                                  <div class="card-body bg-primary">
+                                        <!-- <div class="card border-left-primary shadow h-4"> -->
+                                        <!-- <div class="card-body bg-primary"> -->
 
-                                                  </div>
-                                        </div>
+                                        <!-- </div> -->
+                                        <!-- </div> -->
                               </div>
-                              <h1>Selamat datang di website corona</h1>
+                              <br>
+                              <h1>Selamat datang di website</h1>
                     </div>
           </div>
 
--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -123,34 +123,35 @@
                                         </ul>
                               </li>
 
-                              <li class="nav-item dropdown">
-                                        <a href="#" class="nav-link has-dropdown"><i class="fas fa-bicycle"></i> <span>Features</span></a>
-                                        <ul class="dropdown-menu">
-                                                  <li><a class="nav-link" href="features-activities.html">Activities</a></li>
-                                                  <li><a class="nav-link" href="features-post-create.html">Post Create</a></li>
-                                                  <li><a class="nav-link" href="features-posts.html">Posts</a></li>
-                                                  <li><a class="nav-link" href="features-profile.html">Profile</a></li>
-                                                  <li><a class="nav-link" href="features-settings.html">Settings</a></li>
-                                                  <li><a class="nav-link" href="features-setting-detail.html">Setting Detail</a></li>
-                                                  <li><a class="nav-link" href="features-tickets.html">Tickets</a></li>
+                    <li class="nav-item dropdown">
+                              <a href="#" class="nav-link has-dropdown"><i class="fas fa-bicycle"></i> <span>Features</span></a>
+                              <ul class="dropdown-menu">                                                  
+                              <li><a class="nav-link" href="features-activities.html">Activities</a></li>
+                                        <li><a class="nav-link" href="features-post-create.html">Post Create</a></li>
+                                        <li><a class="nav-link" href="features-posts.html">Posts</a></li>
+                                        <li><a class="nav-link" href="features-profile.html">Profile</a></li>
+                                        <li><a class="nav-link" href="features-settings.html">Settings</a></li>
+                                        <li><a class="nav-link" href="features-setting-detail.html">Setting Detail</a></li>
+                                        <li><a class="nav-link" href="features-tickets.html">Tickets</a></li>
                                         </ul>
                               </li>
-                              <li class="nav-item dropdown">
-                                        <a href="#" class="nav-link has-dropdown"><i class="fas fa-ellipsis-h"></i> <span>Utilities</span></a>
-                                        <ul class="dropdown-menu">
-                                                  <li><a href="utilities-contact.html">Contact</a></li>
-                                                  <li><a class="nav-link" href="utilities-invoice.html">Invoice</a></li>
-                                                  <li><a href="utilities-subscribe.html">Subscribe</a></li>
-                                        </ul>
+<li class="nav-item dropdown">
+                    <a href="#" class="nav-link has-dropdown">
+                              <i class="fas fa-ellipsis-h"></i> <span>Utilities</span></a>
+                    <ul class="dropdown-menu">
+                              <li><a href="utilities-contact.html">Contact</a></li>
+                              <li><a class="nav-link" href="utilities-invoice.html">Invoice</a></li>
+                              <li><a href="utilities-subscribe.html">Subscribe</a></li>
+                   </ul>
                               </li>
                               <!-- <li><a class="nav-link" href="credits.html"><i class="fas fa-pencil-ruler"></i> <span>Credits</span></a>
                               </li> -->
                     </ul>
 
-                    <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-                              <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
-                                        <i class="fas fa-rocket"></i> Documentation
-                              </a>
-                    </div>
+                    <!-- <div class="mt-4 mb-4 p-3 hide-sidebar-mini"> -->
+                              <!-- <!-- <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split"> --> 
+                                        <!-- <i class="fas fa-rocket"></i> Documentation -->
+                              <!-- </a> -->
+                    <!-- </div> -->
           </aside>
 </div>
